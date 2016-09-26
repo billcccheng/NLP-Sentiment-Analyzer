ac35e6d74f79531f62dbea3db2a16324276243de
Bill <iamkg05@gmail.com>
2016-09-26 15:29:39 -0500

    binarized

---
+++ NaiveBayes.py
@@ -0,0 +1,266 @@
+import sys
+import getopt
+import os
+import math
+import operator
+from collections import defaultdict
+
+class NaiveBayes:
+  class TrainSplit:
+    """Represents a set of training/testing data. self.train is a list of Examples, as is self.test. 
+    """
+    def __init__(self):
+      self.train = []
+      self.test = []
+
+  class Example:
+    """Represents a document with a label. klass is 'pos' or 'neg' by convention.
+       words is a list of strings.
+    """
+    def __init__(self):
+      self.klass = ''
+      self.words = []
+
+
+  def __init__(self):
+    """NaiveBayes initialization"""
+    self.FILTER_STOP_WORDS = False
+    self.BOOLEAN_NB = False
+    self.stopList = set(self.readFile('../data/english.stop'))
+    self.numFolds = 10
+    self.NEG_CLASS = 0
+    self.POS_CLASS = 0
+    self.total_num_word_in_pos = 0
+    self.total_num_word_in_neg = 0
+    self.map = defaultdict(lambda: defaultdict(int))
+
+
+  #############################################################################
+  # TODO TODO TODO TODO TODO 
+  # Implement the Multinomial Naive Bayes classifier and the Naive Bayes Classifier with
+  # Boolean (Binarized) features.
+  # If the BOOLEAN_NB flag is true, your methods must implement Boolean (Binarized)
+  # Naive Bayes (that relies on feature presence/absence) instead of the usual algorithm
+  # that relies on feature counts.
+  #
+  #
+  # If any one of the FILTER_STOP_WORDS and BOOLEAN_NB flags is on, the
+  # other one is meant to be off.
+
+  def classify(self, words):
+    """ TODO
+      'words' is a list of words to classify. Return 'pos' or 'neg' classification.
+    """
+    # if not self.BOOLEAN_NB:
+    if self.FILTER_STOP_WORDS:
+      words =  self.filterStopWords(words)
+    # Write code here
+    prior_pos = float(self.POS_CLASS)/(self.POS_CLASS + self.NEG_CLASS)
+    prior_neg = float(self.NEG_CLASS)/(self.POS_CLASS + self.NEG_CLASS)
+    pos_prob, neg_prob = 0, 0
+    for word in words:
+      count_word_in_class_pos = self.map[word]["pos"]
+      #used log to make the data more visualizable
+      pos_prob += math.log(float(count_word_in_class_pos+1) /  self.total_num_word_in_pos)
+
+      count_word_in_class_neg = self.map[word]["neg"]
+      neg_prob += math.log(float(count_word_in_class_neg+1) /  self.total_num_word_in_neg)
+    
+    pos_prob += math.log(prior_pos)
+    neg_prob += math.log(prior_neg)
+
+
+    return "pos" if (pos_prob - neg_prob) > 0 else "neg"
+
+  def addExample(self, klass, words):
+    """
+     * TODO
+     * Train your model on an example document with label klass ('pos' or 'neg') and
+     * words, a list of strings.
+     * You should store whatever data structures you use for your classifier 
+     * in the NaiveBayes class.
+     * Returns nothing
+    """
+    # Write code here
+    if self.FILTER_STOP_WORDS:
+      words =  self.filterStopWords(words)
+    
+    if not self.BOOLEAN_NB:
+      if klass == "pos":
+        self.POS_CLASS+=1  
+      else:
+        self.NEG_CLASS+=1
+
+      for word in words:
+        if klass == "pos":
+          self.total_num_word_in_pos += 1  
+        else: 
+          self.total_num_word_in_neg += 1
+        self.map[word][klass]+=1
+    # This part is for Binarized NB
+    else:
+      for word in words:
+        if klass == "pos":
+          self.POS_CLASS+=1  
+        else:
+          self.NEG_CLASS+=1
+
+      for word in list(set(words)):
+        if klass == "pos":
+          self.total_num_word_in_pos += 1  
+        else: 
+          self.total_num_word_in_neg += 1
+        self.map[word][klass]+=1
+    pass
+      
+
+  # END TODO (Modify code beyond here with caution)
+  #############################################################################
+  
+  
+  def readFile(self, fileName):
+    """
+     * Code for reading a file.  you probably don't want to modify anything here, 
+     * unless you don't like the way we segment files.
+    """
+    contents = []
+    f = open(fileName)
+    for line in f:
+      contents.append(line)
+    f.close()
+    result = self.segmentWords('\n'.join(contents)) 
+    return result
+
+  
+  def segmentWords(self, s):
+    """
+     * Splits lines on whitespace for file reading
+    """
+    return s.split()
+
+  
+  def trainSplit(self, trainDir):
+    """Takes in a trainDir, returns one TrainSplit with train set."""
+    split = self.TrainSplit()
+    posTrainFileNames = os.listdir('%s/pos/' % trainDir)
+    negTrainFileNames = os.listdir('%s/neg/' % trainDir)
+    for fileName in posTrainFileNames:
+      example = self.Example()
+      example.words = self.readFile('%s/pos/%s' % (trainDir, fileName))
+      example.klass = 'pos'
+      split.train.append(example)
+    for fileName in negTrainFileNames:
+      example = self.Example()
+      example.words = self.readFile('%s/neg/%s' % (trainDir, fileName))
+      example.klass = 'neg'
+      split.train.append(example)
+    return split
+
+  def train(self, split):
+    for example in split.train:
+      words = example.words
+      if self.FILTER_STOP_WORDS:
+        words =  self.filterStopWords(words)
+      self.addExample(example.klass, words)
+
+
+  def crossValidationSplits(self, trainDir):
+    """Returns a lsit of TrainSplits corresponding to the cross validation splits."""
+    splits = [] 
+    posTrainFileNames = os.listdir('%s/pos/' % trainDir)
+    negTrainFileNames = os.listdir('%s/neg/' % trainDir)
+    #for fileName in trainFileNames:
+    for fold in range(0, self.numFolds):
+      split = self.TrainSplit()
+      for fileName in posTrainFileNames:
+        example = self.Example()
+        # example.word is all the word in the file
+        example.words = self.readFile('%s/pos/%s' % (trainDir, fileName))
+        example.klass = 'pos'
+        if fileName[2] == str(fold):
+          split.test.append(example)
+        else:
+          split.train.append(example)
+      for fileName in negTrainFileNames:
+        example = self.Example()
+        # example.word is all the word in the file
+        example.words = self.readFile('%s/neg/%s' % (trainDir, fileName))
+        example.klass = 'neg'
+        if fileName[2] == str(fold):
+          split.test.append(example)
+        else:
+          split.train.append(example)
+      splits.append(split)
+    return splits
+  
+  def filterStopWords(self, words):
+    """Filters stop words."""
+    filtered = []
+    for word in words:
+      if not word in self.stopList and word.strip() != '':
+        filtered.append(word)
+    return filtered
+
+def test10Fold(args, FILTER_STOP_WORDS, BOOLEAN_NB):
+  nb = NaiveBayes()
+  #splits = split(train(example(klass, words)), test(example(klass, words)))
+  splits = nb.crossValidationSplits(args[0])
+  # print splits[0].train[0].words
+  avgAccuracy = 0.0
+  fold = 0
+  for split in splits:
+    classifier = NaiveBayes()
+    classifier.FILTER_STOP_WORDS = FILTER_STOP_WORDS
+    classifier.BOOLEAN_NB = BOOLEAN_NB
+    accuracy = 0.0
+    for example in split.train:
+      words = example.words
+      classifier.addExample(example.klass, words)
+  
+    for example in split.test:
+      words = example.words
+      guess = classifier.classify(words)
+      if example.klass == guess:
+        accuracy += 1.0
+
+    accuracy = accuracy / len(split.test)
+    avgAccuracy += accuracy
+    print '[INFO]\tFold %d Accuracy: %f' % (fold, accuracy) 
+    fold += 1
+  avgAccuracy = avgAccuracy / fold
+  print '[INFO]\tAccuracy: %f' % avgAccuracy
+    
+    
+def classifyDir(FILTER_STOP_WORDS, BOOLEAN_NB, trainDir, testDir):
+  classifier = NaiveBayes()
+  classifier.FILTER_STOP_WORDS = FILTER_STOP_WORDS
+  classifier.BOOLEAN_NB = BOOLEAN_NB
+  trainSplit = classifier.trainSplit(trainDir)
+  classifier.train(trainSplit)
+  testSplit = classifier.trainSplit(testDir)
+  accuracy = 0.0
+  for example in testSplit.train:
+    words = example.words
+    guess = classifier.classify(words)
+    if example.klass == guess:
+      accuracy += 1.0
+  accuracy = accuracy / len(testSplit.train)
+  print '[INFO]\tAccuracy: %f' % accuracy
+
+
+def main():
+  FILTER_STOP_WORDS = False
+  BOOLEAN_NB = False
+  (options, args) = getopt.getopt(sys.argv[1:], 'fbm')
+  if ('-f','') in options:
+    FILTER_STOP_WORDS = True
+  elif ('-b','') in options:
+    BOOLEAN_NB = True
+  
+  if len(args) == 2:
+    classifyDir(FILTER_STOP_WORDS, BOOLEAN_NB,  args[0], args[1])
+  elif len(args) == 1:
+    test10Fold(args, FILTER_STOP_WORDS, BOOLEAN_NB)
+
+if __name__ == "__main__":
+    main()
---
+++ Perceptron.py
@@ -0,0 +1,251 @@
+import sys
+import getopt
+import os
+import math
+import operator
+import random
+from collections import defaultdict
+import copy
+
+class Perceptron:
+  class TrainSplit:
+    """Represents a set of training/testing data. self.train is a list of Examples, as is self.test. 
+    """
+    def __init__(self):
+      self.train = []
+      self.test = []
+
+  class Example:
+    """Represents a document with a label. klass is 'pos' or 'neg' by convention.
+       words is a list of strings.
+    """
+    def __init__(self):
+      self.klass = ''
+      self.words = []
+
+
+  def __init__(self):
+    """Perceptron initialization"""
+    #in case you found removing stop words helps.
+    self.stopList = set(self.readFile('../data/english.stop'))
+    self.numFolds = 10
+    # self.map = defaultdict(int)
+    self.weights = defaultdict(int)
+    self.input = []
+    self.output = []
+    self.threshold = 0
+    self.lrate = 0.0005
+
+  #############################################################################
+  # TODO TODO TODO TODO TODO 
+  # Implement the Perceptron classifier with
+  # the best set of features you found through your experiments with Naive Bayes.
+
+  def classify(self, words):
+    """ TODO
+      'words' is a list of words to classify. Return 'pos' or 'neg' classification.
+    """
+    # Write code here
+    words = self.filterStopWords(words)
+    classify_map = defaultdict(int)
+    for word in words:
+      classify_map[word]=1
+      # classify_map[word]+=1
+    # self.normalize(classify_map)
+    return "pos" if self.output_res(classify_map) >= self.threshold else "neg"
+  
+
+  def addExample(self, klass, words):
+    """
+     * TODO
+     * Train your model on an example document with label klass ('pos' or 'neg') and
+     * words, a list of strings.
+     * You should store whatever data structures you use for your classifier 
+     * in the Perceptron class.
+     * Returns nothing
+    """
+
+    # Write code here
+    words = self.filterStopWords(words)
+    word_map = defaultdict(int) 
+    for word in words:
+      if word not in word_map:
+        word_map[word] = 1
+        # word_map[word] += 1
+      if word not in self.weights:
+        self.weights[word] = random.random()
+    # self.normalize(word_map)
+    self.input.append(copy.deepcopy(word_map))
+    self.output.append(1 if klass == "pos" else -1) 
+
+    pass
+  
+  def train(self, split, iterations):
+    """
+    * TODO 
+    * iterates through data examples
+    * TODO 
+    * use weight averages instead of final iteration weights
+    """
+    for example in split.train:
+      words = example.words
+      self.addExample(example.klass, words)
+
+    for i in range(iterations):
+      totalError = 0
+      for j in range(len(self.output)):
+        out_res = self.output_res(self.input[j])
+        error  = self.output[j] - out_res
+
+        totalError+=error
+        for word in self.input[j]:
+          # print word
+          # sys.exit("Break")
+          delta = self.lrate * self.input[j][word] * error
+          self.weights[word] += delta
+      if not totalError:
+        break
+
+  def output_res(self, _input):
+    _sum = 0.0   
+    for word in _input:
+      _sum+=self.weights[word]*_input[word]
+    return 1 if _sum > self.threshold else -1
+
+
+  def normalize(self, words):
+    factor=1.0/sum(words.itervalues())
+    for k in words:
+        words[k] = words[k]*factor
+
+  # END TODO (Modify code beyond here with caution)
+  #############################################################################
+  
+  
+  def readFile(self, fileName):
+    """
+     * Code for reading a file.  you probably don't want to modify anything here, 
+     * unless you don't like the way we segment files.
+    """
+    contents = []
+    f = open(fileName)
+    for line in f:
+      contents.append(line)
+    f.close()
+    result = self.segmentWords('\n'.join(contents)) 
+    return result
+
+  
+  def segmentWords(self, s):
+    """
+     * Splits lines on whitespace for file reading
+    """
+    return s.split()
+
+  
+  def trainSplit(self, trainDir):
+    """Takes in a trainDir, returns one TrainSplit with train set."""
+    split = self.TrainSplit()
+    posTrainFileNames = os.listdir('%s/pos/' % trainDir)
+    negTrainFileNames = os.listdir('%s/neg/' % trainDir)
+    for fileName in posTrainFileNames:
+      example = self.Example()
+      example.words = self.readFile('%s/pos/%s' % (trainDir, fileName))
+      example.klass = 'pos'
+      split.train.append(example)
+    for fileName in negTrainFileNames:
+      example = self.Example()
+      example.words = self.readFile('%s/neg/%s' % (trainDir, fileName))
+      example.klass = 'neg'
+      split.train.append(example)
+    return split
+
+
+  def crossValidationSplits(self, trainDir):
+    """Returns a lsit of TrainSplits corresponding to the cross validation splits."""
+    splits = [] 
+    posTrainFileNames = os.listdir('%s/pos/' % trainDir)
+    negTrainFileNames = os.listdir('%s/neg/' % trainDir)
+    #for fileName in trainFileNames:
+    for fold in range(0, self.numFolds):
+      split = self.TrainSplit()
+      for fileName in posTrainFileNames:
+        example = self.Example()
+        example.words = self.readFile('%s/pos/%s' % (trainDir, fileName))
+        example.klass = 'pos'
+        if fileName[2] == str(fold):
+          split.test.append(example)
+        else:
+          split.train.append(example)
+      for fileName in negTrainFileNames:
+        example = self.Example()
+        example.words = self.readFile('%s/neg/%s' % (trainDir, fileName))
+        example.klass = 'neg'
+        if fileName[2] == str(fold):
+          split.test.append(example)
+        else:
+          split.train.append(example)
+      splits.append(split)
+    return splits
+  
+  
+  def filterStopWords(self, words):
+    """Filters stop words."""
+    filtered = []
+    for word in words:
+      if not word in self.stopList and word.strip() != '':
+        filtered.append(word)
+    return filtered
+
+def test10Fold(args):
+  pt = Perceptron()
+  
+  iterations = int(args[1])
+  splits = pt.crossValidationSplits(args[0])
+  avgAccuracy = 0.0
+  fold = 0
+  for split in splits:
+    classifier = Perceptron()
+    accuracy = 0.0
+    classifier.train(split,iterations)
+  
+    for example in split.test:
+      words = example.words
+      guess = classifier.classify(words)
+      if example.klass == guess:
+        accuracy += 1.0
+
+    accuracy = accuracy / len(split.test)
+    avgAccuracy += accuracy
+    print '[INFO]\tFold %d Accuracy: %f' % (fold, accuracy) 
+    fold += 1
+  avgAccuracy = avgAccuracy / fold
+  print '[INFO]\tAccuracy: %f' % avgAccuracy
+    
+    
+def classifyDir(trainDir, testDir,iter):
+  classifier = Perceptron()
+  trainSplit = classifier.trainSplit(trainDir)
+  iterations = int(iter)
+  classifier.train(trainSplit,iterations)
+  testSplit = classifier.trainSplit(testDir)
+  #testFile = classifier.readFile(testFilePath)
+  accuracy = 0.0
+  for example in testSplit.train:
+    words = example.words
+    guess = classifier.classify(words)
+    if example.klass == guess:
+      accuracy += 1.0
+  accuracy = accuracy / len(testSplit.train)
+  print '[INFO]\tAccuracy: %f' % accuracy
+    
+def main():
+  (options, args) = getopt.getopt(sys.argv[1:], '')
+  
+  if len(args) == 3:
+    classifyDir(args[0], args[1], args[2])
+  elif len(args) == 2:
+    test10Fold(args)
+
+if __name__ == "__main__":
+    main()
